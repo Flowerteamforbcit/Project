13502120755bb23cf07512076afca89ffa6ea10f
Harryki <zgp7777@gmail.com>
2016-05-23 16:53:32 -0700

    favicon

--- header.php
+++ header.php
@@ -7,12 +7,13 @@
   <link rel="stylesheet" href="css/style.css" />  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
   integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    
+
   <link rel="stylesheet" href="css/jquery.rateyo.css"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script type="text/javascript" src="js/products.js"></script>
 
+  <link rel="icon" href="img/favico.png" type="image/x-icon" />
   
 </head>
 
