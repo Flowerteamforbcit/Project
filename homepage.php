7665c492db84debbcc3a39d76dd405af8039984f
Viktor <purple_goth@bk.ru>
2016-05-23 14:18:51 -0700

    31
    13

--- homepage.php
+++ homepage.php
@@ -2,33 +2,24 @@
 session_start();
 include 'header.php';
 ?>
+    <link rel="stylesheet" href="css/home.css" />
+    <link href='http://fonts.googleapis.com/css?family=Oleo+Script' rel='stylesheet' type='text/css'>
 
+<body>
+    <img src="img/home.jpg" id="full-screen-background-image"/>
 
-<img src="img/home.jpg" id="full-screen-background-image"/>
+            <div class="container text-center" id="homepage" style="min-height: 80%; text-align: center; ">
 
+                <h2>If you are looking for building supplies<br/>
+                with the best prices. You are on the right place...</h2>
+                <div style="margin-top: 30%"><a href="index.php" class="btn btn-default btn-sq btn-transparent btn-noboard" role="button" ><img src="img/search.png"/><br/><h2>Search </h2></a></div>
+                <?php  if (!(isset($_SESSION['userEmail']))) echo '<br/> <div style="margin-top: 30%"><a href="login.php" class="btn btn-warning" role="button">Sign in</a>
+        <br/><a href="login.php" >Join</a></div>'; ?>
+                <?php if (isset($_SESSION['userEmail'])) echo ' <div style="margin-top: 30%"><h2>Hi ' . $_SESSION['userEmail'].'!</h2></div>';?>
 
-<div class="jumbotron" id="homepage">
+	</div>
 
-	
-	
-	<?php if (!array_key_exists('userEmail', $_SESSION)) echo '<h1>Signup today</h1><br><a href="signup.php" class="btn btn-info" role="button">Sign up</a>'; ?>
-	<?php if (array_key_exists('userEmail', $_SESSION)) echo '<h1>Hi ' . $_SESSION['userEmail'].'!</h1>'; 
-
-	?>
-	<h2><a href="index.php" >Go to search</a></h2>
-</div>
-
-<div class="jumbotron" id="news">
-	<h2>Lated news</h2>
-	<p>The practice of writing paragraphs is essential to good writing. Paragraphs help to break up large chunks of text and makes the content easier for readers to digest. They guide the reader through your argument by focusing on one main idea or goal.</p>
-</div>
-<div class="jumbotron" id="nothing">
-	<h2>sample image</h2>
-
-	<img src="img/racks.jpg"/>
-
-</div>
-
+</body>
 
 
 <?php
